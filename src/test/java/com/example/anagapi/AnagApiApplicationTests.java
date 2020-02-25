c70888544ee2ed1d7cfd2b40767d56b56863558a
YevgenP <yevgenp@Home>
2020-02-25 16:39:54 +0200

    Initial skeleton created.

---
+++ src/test/java/com/example/anagapi/AnagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
